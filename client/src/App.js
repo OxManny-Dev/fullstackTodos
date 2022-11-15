d4028667a05662e1241c240183b7b41f811ff571
OxManny-Dev <emmanueljucaban@gmail.com>
2022-11-14 21:55:12 -0500

    init repo

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+// Functional components are functions
+// that return html
+
+
+const App = () => <h1 className='container manny weirdo'>Hello World</h1>;
+
+// Letting other files such as index.js use it
+// module.exports = App;
+export default App;
+
+
