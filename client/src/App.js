113fa5e14cd1174c4508df41fad59cfb859db870
oxjukefinite <OxJukeFinity@gmail.com>
2022-11-28 22:18:23 -0500

    Update repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import {
   Todo,
 } from './Todo';
@@ -8,9 +10,40 @@ import {
 
 //  attributes === props
 
+// For React to make UI updates to the UI,
+// we need to trigger a "state" change
+// When a components state changes,
+// that component and all of its children
+// will re-render
+
+
 const App = () => {
+
+  // useState returns an array with 2 elements
+  // 1st element = whatever pass as a parameter to useState
+  // 2nd element = is a function for changing the value of the 1st element
+  const [count, setCount] = useState(0);
+
+
   return (
     <div className='green'>
+      <p>Counter: {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          }
+        }}
+      >
+        Decrement
+      </button>
       <Todo
         name={'Manny'}
         todos={[
@@ -19,14 +52,15 @@ const App = () => {
           'Pay the bills',
         ]}
       />
-      <Todo
-        name={'Joseph'}
-        todos={[
-          'Wake up',
-          'Cry',
-          'Sleep',
-        ]}
-      />
+      {/*<Todo*/}
+      {/*  name={'Joseph'}*/}
+      {/*  todos={[*/}
+      {/*    'Wake up',*/}
+      {/*    'Cry',*/}
+      {/*    'Sleep',*/}
+      {/*  ]}*/}
+      {/*/>*/}
+
       <h1 className='container manny weirdo'>Hello World</h1>
     </div>
   );
--- client/src/Todo/Todo.js
+++ client/src/Todo/Todo.js
@@ -1,29 +1,83 @@
+import {useState} from 'react';
 // We can export multiple things
 // by just using the export keyword per file
 
 // the catch is that we can only
 // have 1 default export per file
 
-export const Todo = ({ name, todos}) => {
+const ourBlue = '#0000ee';
+
+// For React to make UI updates to the UI,
+// we need to trigger a "state" change
+//When a components state changes,
+// that component and all of its children
+// will re-render
 
 
+export const Todo = ({name,}) => {
+
+  const [todoInput, setTodoInput] = useState('');
+
+  const [todos, setTodos] = useState([]);
+
+  // Tip use array method splice
 
   return (
     <div>
       <h1>This is the Todo App</h1>
       <p>Hi my name is {name} and these are my todos</p>
+      <input
+        value={todoInput}
+        onChange={(event) => {
+          setTodoInput(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const newTodos = [...todos, todoInput];
+          setTodos(newTodos);
+          setTodoInput('');
+          //   When the user clicks on the submit button
+          //  update the todos state with the todoInput
+          //  Catch: you cannot modify todos directly
+          //  bonus: after the todo is added, reset the todoInput back to nothing
+        }}
+      >
+        Submit
+      </button>
+
       <ul>
         {
           todos.length === 0 ?
             <h1>No Todos Yet</h1>
             :
-            todos.map(todo => <li>{todo}</li>)
+            todos.map((todo, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{
+                    color: index % 2 === 0 ? ourBlue : 'green',
+                    display: 'flex'
+                  }}
+                >
+                  <li>{todo}</li>
+                  <button
+                    onClick={() => {
+                      const newTodos = [...todos];
+                      newTodos.splice(index, 1);
+                      setTodos(newTodos);
+                    }}
+                  >
+                    X
+                  </button>
+                </div>
+              );
+            })
         }
       </ul>
     </div>
   );
 };
-
 
 
 
